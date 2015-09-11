4827bc3655e184f3bf263eb6798e06075bb70741
Francesco <xhaferri@msn.com>
2015-09-11 23:33:34 +0200

    databases updated

--- dmipreprints.sql
+++ dmipreprints.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.12deb2
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mag 24, 2015 alle 14:58
--- Versione del server: 5.6.24-0ubuntu2
--- PHP Version: 5.6.4-4ubuntu6
+-- Generato il: Set 11, 2015 alle 23:32
+-- Versione del server: 5.5.44-0ubuntu0.14.04.1
+-- Versione PHP: 5.5.9-1ubuntu4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,17 +27,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `AUTORI` (
-  `nome` varchar(100) NOT NULL
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`nome`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dump dei dati per la tabella `AUTORI`
---
-
-INSERT INTO `AUTORI` (`nome`) VALUES
-('Bruno Iannazzo'),
-('Gianluca Vinti'),
-('Laura Angeloni');
 
 -- --------------------------------------------------------
 
@@ -46,17 +38,9 @@ INSERT INTO `AUTORI` (`nome`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `AUTORI_BACKUP` (
-  `nome` varchar(100) NOT NULL
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`nome`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dump dei dati per la tabella `AUTORI_BACKUP`
---
-
-INSERT INTO `AUTORI_BACKUP` (`nome`) VALUES
-('Bruno Iannazzo'),
-('Gianluca Vinti'),
-('Laura Angeloni');
 
 -- --------------------------------------------------------
 
@@ -65,7 +49,8 @@ INSERT INTO `AUTORI_BACKUP` (`nome`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `DATA_ULTIMO_LANCIO` (
-  `data` varchar(100) NOT NULL
+  `data` varchar(100) NOT NULL,
+  PRIMARY KEY (`data`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -73,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `DATA_ULTIMO_LANCIO` (
 --
 
 INSERT INTO `DATA_ULTIMO_LANCIO` (`data`) VALUES
-('2015-05-24 14:55');
+('2015-09-11 23:26');
 
 -- --------------------------------------------------------
 
@@ -92,7 +77,10 @@ CREATE TABLE IF NOT EXISTS `PREPRINTS` (
   `categoria` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `abstract` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `Filename` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci,
-  `checked` varchar(1) NOT NULL DEFAULT '0'
+  `checked` varchar(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `counter` bigint(255) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_pubblicazione`),
+  FULLTEXT KEY `id_pubblicazione` (`id_pubblicazione`,`titolo`,`data_pubblicazione`,`autori`,`referenze`,`commenti`,`categoria`,`abstract`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -112,7 +100,10 @@ CREATE TABLE IF NOT EXISTS `PREPRINTS_ARCHIVIATI` (
   `categoria` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `abstract` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `Filename` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci,
-  `checked` varchar(1) NOT NULL DEFAULT '0'
+  `checked` varchar(1) NOT NULL DEFAULT '0',
+  `counter` bigint(255) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_pubblicazione`),
+  FULLTEXT KEY `id_pubblicazione` (`id_pubblicazione`,`titolo`,`data_pubblicazione`,`autori`,`referenze`,`commenti`,`categoria`,`abstract`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -122,7 +113,8 @@ CREATE TABLE IF NOT EXISTS `PREPRINTS_ARCHIVIATI` (
 --
 
 CREATE TABLE IF NOT EXISTS `sessione` (
-  `attivo` varchar(1) NOT NULL
+  `attivo` varchar(1) NOT NULL,
+  PRIMARY KEY (`attivo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -139,7 +131,8 @@ INSERT INTO `sessione` (`attivo`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `sessione_data` (
-  `data` varchar(100) NOT NULL
+  `data` varchar(100) NOT NULL,
+  PRIMARY KEY (`data`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -147,7 +140,7 @@ CREATE TABLE IF NOT EXISTS `sessione_data` (
 --
 
 INSERT INTO `sessione_data` (`data`) VALUES
-('20150524');
+('20150911');
 
 -- --------------------------------------------------------
 
@@ -156,60 +149,9 @@ INSERT INTO `sessione_data` (`data`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `temp` (
-  `id` varchar(100) NOT NULL
+  `id` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `AUTORI`
---
-ALTER TABLE `AUTORI`
- ADD PRIMARY KEY (`nome`);
-
---
--- Indexes for table `AUTORI_BACKUP`
---
-ALTER TABLE `AUTORI_BACKUP`
- ADD PRIMARY KEY (`nome`);
-
---
--- Indexes for table `DATA_ULTIMO_LANCIO`
---
-ALTER TABLE `DATA_ULTIMO_LANCIO`
- ADD PRIMARY KEY (`data`);
-
---
--- Indexes for table `PREPRINTS`
---
-ALTER TABLE `PREPRINTS`
- ADD PRIMARY KEY (`id_pubblicazione`), ADD FULLTEXT KEY `id_pubblicazione` (`id_pubblicazione`,`titolo`,`data_pubblicazione`,`autori`,`referenze`,`commenti`,`categoria`,`abstract`);
-
---
--- Indexes for table `PREPRINTS_ARCHIVIATI`
---
-ALTER TABLE `PREPRINTS_ARCHIVIATI`
- ADD PRIMARY KEY (`id_pubblicazione`), ADD FULLTEXT KEY `id_pubblicazione` (`id_pubblicazione`,`titolo`,`data_pubblicazione`,`autori`,`referenze`,`commenti`,`categoria`,`abstract`);
-
---
--- Indexes for table `sessione`
---
-ALTER TABLE `sessione`
- ADD PRIMARY KEY (`attivo`);
-
---
--- Indexes for table `sessione_data`
---
-ALTER TABLE `sessione_data`
- ADD PRIMARY KEY (`data`);
-
---
--- Indexes for table `temp`
---
-ALTER TABLE `temp`
- ADD PRIMARY KEY (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
